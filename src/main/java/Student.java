5e7ca2143ca2ea663253690c8cab86f3ee97d52e
jamin-xie-ics4u1-02-25 <Jamin.xie@student.tdsb.on.ca>
2025-05-16 03:22:20 +0000

    Update Change

---
+++ src/main/java/Student.java
@@ -0,0 +1,46 @@
+import java.util.Objects;
+
+public class Student {
+    private String name;
+    private int age;
+    private String studentNumber;
+
+    public Student(String name, int age, String studentNumber) {
+        this.name = name;
+        this.age = age;
+        this.studentNumber = studentNumber;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public String getStudentNumber() {
+        return this.studentNumber;
+    }
+
+    @Override
+    public String toString() {
+        return this.name + ", " + this.age + " - " + this.studentNumber;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Student student = (Student)obj;
+        return student.getStudentNumber().equals(this.studentNumber);
+    }
+
+    public int hasCode() {
+        return Objects.hash(studentNumber);
+    }
+}
